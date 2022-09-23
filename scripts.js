a10f57ad6ddb50f9fb1be49ae0a0f82d80e0fcb1
Nikolai Whittaker <nikolai.whitt@gmail.com>
2022-09-23 16:31:09 -0400

    finished major functionality, completed styling

--- scripts.js
+++ scripts.js
@@ -1,10 +1,95 @@
+import {replyArray, triggerArray} from './responses.js'
+
+
 //Main scripts for NikolAI-chatbot
+const input = document.getElementById('textInput');
+
+//Timeout
+function getRandomTimeout() {
+    return Math.random() * (1000 - 500) + 500;
+  }
+
+//Event listener for chat input
+//Logs input sends to main function and clears input
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#textInput').addEventListener('keydown', function(e) {
     if(e.code == 'Enter'){
-        console.log(document.querySelector('#textInput').value)
+        let inputValue = input.value;
+        inputValue = regexReplace(inputValue);
+        let rawString = input.value;
+        console.log(inputValue);
+        input.value = '';
+        let reply = compare(replyArray, triggerArray, inputValue);
+        console.log(reply);
+        updateuserChat(rawString);
+        window.scrollTo(0, document.body.scrollHeight);
+        console.log(getRandomTimeout());
+        setTimeout(function() { updatebotChat(reply);window.scrollTo(0, document.body.scrollHeight); }, getRandomTimeout());
+        
+        
     }
     });
 
 }
 );
+
+const regexReplace = (text) => {
+
+    text = text.toLowerCase().replace(/[^\w\s\d]/gi, "");
+    text = text
+        .replace(/ a /g, " ")
+        .replace(/i feel /g, "")
+        .replace(/whats/g, "what is")
+        .replace(/please /g, "")
+        .replace(/ please/g, "");
+    return text;
+};
+
+//function to compare input text to an array of triggers and return the matching array.
+const compare = (replyArray, triggerArray, text ) => {
+    let replytext = "";
+    let isValid = false;
+    for(let i = 0; i< triggerArray.length; i++){
+
+        let innerArray = triggerArray[i];
+
+        for(let j=0; j<=innerArray.length; j++){
+            if(text.includes(triggerArray[i][j])){
+                replytext = replyArray[i];
+                isValid = true;
+            
+
+            }
+        }
+    }
+    if (isValid){
+        return replytext;
+    }
+    return 'I have not reached such intelligence levels to understand what you are saying. :('
+    
+        
+    
+}
+
+const updateuserChat = (text) => {
+    const chatDiv = document.getElementById('messages');
+    let userChat = document.createElement('div');
+    let botChat = document.createElement('div');
+    userChat.id = 'user';
+    botChat.id = 'bot';
+    userChat.innerHTML = `<p class='user'>User : <span>${text} </span></p>`;
+    chatDiv.append(userChat);
+    
+
+}
+
+const updatebotChat = ( reply) => {
+    const chatDiv = document.getElementById('messages');
+    let botChat = document.createElement('div');
+    botChat.id = 'bot';
+    botChat.innerHTML = `<p>User : <span>${reply} </span></p>`;
+    chatDiv.append(botChat);
+
+}
+
+
